9146dc5c48a4d8ac537105973b89d51a7571b01f
Raphael Shirley <Raphael.Shirley@sussex.ac.uk>
2017-05-30 18:00:26 +0100

    minor changes before end of day

--- dmu4/generatemasks.py
+++ dmu4/generatemasks.py
@@ -1,34 +1,34 @@
 from herschelhelp_internal import starmask
 
 fields= [
-'AKARI-NEP.fits',
-'AKARI-SEP.fits',
-'Bootes.fits',
-'CDFS-SWIRE.fits',
-'COSMOS.fits',
-'EGS.fits',
-'ELAIS-N1.fits',
-'ELAIS-N2.fits,
-'ELAIS-S1.fits','
-'GAMA-09.fits',
-'GAMA-12.fits',
-'GAMA-15.fits',
-'HDF-N.fits',
-'Herschel-Stripe-82.fits',
-'Lockman-SWIRE.fits',
-'NGP.fits',
-'SA13.fits',
-'SGP.fits',
-'SPIRE-NEP.fits',
-'SSDF.fits',
-'xFLS.fits',
+'AKARI-NEP',
+'AKARI-SEP',
+'Bootes',
+'CDFS-SWIRE',
+'COSMOS',
+'EGS',
+'ELAIS-N1',
+'ELAIS-N2',
+'ELAIS-S1',
+'GAMA-09',
+'GAMA-12',
+'GAMA-15',
+'HDF-N',
+'Herschel-Stripe-82',
+'Lockman-SWIRE',
+'NGP',
+'SA13',
+'SGP',
+'SPIRE-NEP',
+'SSDF',
+'xFLS',
 'XMM-13hr',
 'XMM-LSS']
 
 
 for field in fields:
     starmask.create_starmask('../dmu0/dmu0_GAIA/data/GAIA_' + field + '.fits',
-                             'dmu4_starmasks/starmask-GAIA_' + field + '.reg')
+                             'dmu4_starmasks/starmask-' + field + '.reg')
     
     #starmask.ds9tomoc('dmu4_starmasks/starmask-GAIA_' + field + '.reg',
     #                  'dmu4_starmasks/starmask-GAIA_' + field + '_MOC.fits')
