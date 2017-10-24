ee9d1d90887eb41c0d7741370b4d896c03059a6c
Pete Hurley <p.d.hurley@sussex.ac.uk>
2017-10-24 11:05:24 +0100

    XID+PACS_CDFS-SWIRE complete

---
+++ dmu26/dmu26_XID+PACS_CDFS-SWIRE/XID_plus_hier.sh
@@ -0,0 +1,12 @@
+/bin/bash
+#$ -S /bin/bash
+#$ -o /lustre/scratch/astro/pdh21/log/out
+#$ -e /lustre/scratch/astro/pdh21/log/err
+cd /lustre/scratch/astro/pdh21/CDFS-SWIRE/PACS/output/
+echo "this is from the run script"
+
+module load use.own
+module load fir/software
+export PATH="/research/astro/fir/HELP/help_python/miniconda3/bin/"
+
+python -c 'from xidplus import HPC;HPC.hierarchical_tile("Master_prior.pkl", "Tiles.pkl")'
---
+++ dmu26/dmu26_XID+PACS_CDFS-SWIRE/XID_plus_tile.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#$ -S /bin/bash
+#$ -o /lustre/scratch/astro/pdh21/log/out
+#$ -e /lustre/scratch/astro/pdh21/log/err
+cd /lustre/scratch/astro/pdh21/CDFS-SWIRE/PACS/
+echo "this is from the run script"
+
+module load use.own
+module load fir/software
+export PATH="/research/astro/fir/HELP/help_python/miniconda3/bin/":$PATH
+
+python XIDp_run_script_pacs_tile.py
