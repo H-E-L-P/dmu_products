1a0faa8558dabbc6692282f9dd52bf86304bfbb4
Maria <mccampos7@gmail.com>
2018-11-29 14:24:29 +0000

    Update ELAIS-N2

---
+++ dmu26/dmu26_XID+SPIRE_ELAIS-N2/XID_plus_tile.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#$ -S /bin/bash
+#$ -o /lustre/scratch/astro/mc741/log/out
+#$ -e /lustre/scratch/astro/mc741/log/err
+cd /lustre/scratch/astro/mc741/dmu_products/dmu26/dmu26_XID+SPIRE_ELAIS-N2/data/
+echo "this is from the run script"
+
+module load use.own
+module load fir/software
+export PATH="/research/astro/fir/HELP/help_python/miniconda3/bin/":$PATH
+
+python XIDp_run_script_spire_tile.py
