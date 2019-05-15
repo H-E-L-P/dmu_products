fb4946b2eb4d9f5d1d67b503a1a46dfddb081e27
Maria <mccampos7@gmail.com>
2019-05-15 12:32:08 +0100

    update AKARI-NEP

--- dmu26/dmu26_XID+MIPS_AKARI-NEP/XID_plus_tile.sh
+++ dmu26/dmu26_XID+MIPS_AKARI-NEP/XID_plus_tile.sh
@@ -2,11 +2,15 @@
 #$ -S /bin/bash
 #$ -o /its/home/mc741/git_hub/dmu_products/log/out
 #$ -e /its/home/mc741/git_hub/dmu_products/log/err
-cd /its/home/mc741/git_hub/dmu_products/dmu26/dmu26_XID+MIPS_AKARI-NEP
+#cd /its/home/mc741/git_hub/dmu_products/dmu26/dmu26_XID+MIPS_AKARI-NEP
 echo "this is from the run script"
 
 module load use.own
 module load fir/software
 export PATH="/research/astro/fir/HELP/help_python/miniconda3/bin/":$PATH
 
-python XIDp_run_script_mips_tile.py
+echo $(pwd)
+path_here
+echo $(pwd)
+
+python ../../../XIDp_run_script_mips_tile.py 
