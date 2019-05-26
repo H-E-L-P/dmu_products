b1443d04ed243fcf0f72c8143fbda716211bbc9d
Maria <mccampos7@gmail.com>
2019-05-26 12:23:35 +0100

    update MIPS HDF-N

--- dmu26/dmu26_XID+MIPS_HDF-N/XID_plus_hier.sh
+++ dmu26/dmu26_XID+MIPS_HDF-N/XID_plus_hier.sh
@@ -9,4 +9,10 @@ module load use.own
 module load fir/software
 export PATH="/research/astro/fir/HELP/help_python/miniconda3/bin/"
 
+
+echo $(pwd)
+path_here
+echo $(pwd)
+
+
 python -c 'from xidplus import HPC;import sys; sys.path.remove("/mnt/pact/im281/HELP/XID_plus");sys.path.remove("/mnt/pact/im281/HELP/herschelhelp_python");HPC.hierarchical_tile("Master_prior.pkl", "Tiles.pkl")'
