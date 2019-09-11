599fbaa8cffa670f85e452c0fb6f9dd1d96a3d5d
Maria <mccampos7@gmail.com>
2019-09-11 13:28:01 +0100

    update dmu17

--- dmu17/dmu17_HELP_Legacy_Maps/ELAIS-N1/README.md
+++
@@ -1,8 +0,0 @@
-### dmu17_ELAIS-N1
-
-### PSF normalisation
-PSF normalisation for ELAIS-N1 MIPS map was carried out using Herve Aussel's Jupyter notebook 
-[normalize_MIPS_psf.ipynb](./normalize_MIPS_psf.ipynb).
-
-The final normalised PSF is:
-`dmu17_MIPS_PSF_ELAIS-N1_20170818.fits`
---
+++ dmu17/dmu17_Legacy_maps/EGS/wget_data.bat
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# To run as an executable on a unix platform, do the following:
+# chmod 775 wget_data.bat
+# ./wget_data.bat
+#
+wget -x "https://irsa.ipac.caltech.edu//data/SPITZER/FIDEL/images/fidel_egs_24_s1plus_v0.50_sci.fits"
+wget -x "https://irsa.ipac.caltech.edu//data/SPITZER/FIDEL/images/fidel_egs_24_s1plus_v0.50_std.fits"
+wget -x "https://irsa.ipac.caltech.edu//data/SPITZER/FIDEL/images/fidel_egs_24_s1plus_v0.50_exp.fits"
+wget -x "https://irsa.ipac.caltech.edu//data/SPITZER/FIDEL/images/fidel_egs_70_s1plus_v0.50_sci.fits"
+wget -x "https://irsa.ipac.caltech.edu//data/SPITZER/FIDEL/images/fidel_egs_70_s1plus_v0.50_std.fits"
+wget -x "https://irsa.ipac.caltech.edu//data/SPITZER/FIDEL/images/fidel_egs_70_s1plus_v0.50_exp.fits"
+wget -x "https://irsa.ipac.caltech.edu//data/SPITZER/FIDEL/images/fidel_egs_160_s1plus_v0.50_sci.fits"
+wget -x "https://irsa.ipac.caltech.edu//data/SPITZER/FIDEL/images/fidel_egs_160_s1plus_v0.50_std.fits"
+wget -x "https://irsa.ipac.caltech.edu//data/SPITZER/FIDEL/images/fidel_egs_160_s1plus_v0.50_exp.fits"
